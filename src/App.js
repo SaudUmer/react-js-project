8af766af6b8efc7c804ee5ff48aabd86f014b6f9
SaudUmer <saudmamdani827@gmail.com>
2023-08-28 14:32:07 +0500

    js commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+// import React from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//     <MyName/>
+//       <h1 className="MyClass">Hello World {54 - 24}</h1>
+//       <p>Awesome</p>
+//       <img src="" alt="" />
+
+//     </>
+//   );
+// };
+
+// const MyName = () => {
+//    return <h1>Hamdan Groups</h1>
+// }
+
+// //how react is actually looking into it 
+
+// // const App = () => {
+// //   return React.createElement("h1", {}, "Hello World")
+// // }
+
+// export default App;
+
+
+import React from "react";
+import Restaurant from "./Components/Basics/Restaurant";
+
+const App = () => {
+  return <Restaurant/>
+}
+
+export default App
